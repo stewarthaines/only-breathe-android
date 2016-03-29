1163891ff232384c2f159b1772d66567397ae4e1
Stewart Haines <stewarthaines@gmail.com>
2016-03-29 16:11:40 +1100

    adds display of selected file name, size

--- app/src/main/java/com/robotinabox/onlybreathe/MainActivity.java
+++ app/src/main/java/com/robotinabox/onlybreathe/MainActivity.java
@@ -1,15 +1,27 @@
 package com.robotinabox.onlybreathe;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mSelectVideoBtn;
+    private static final int REQUEST_CODE = 1;
+    private TextView mDurationDimension;
+    private TextView mFilenameDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +38,67 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        mSelectVideoBtn = (Button) findViewById(R.id.select_video_btn);
+        mSelectVideoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectVideoWithIntent();
+            }
+        });
+
+        mDurationDimension = (TextView) findViewById(R.id.duration_dimension);
+
+        mFilenameDate = (TextView) findViewById(R.id.filename_date);
+    }
+
+    private void selectVideoWithIntent() {
+
+        Intent mediaChooser = new Intent(Intent.ACTION_GET_CONTENT);
+        //comma-separated MIME types
+        mediaChooser.setType("video/*");
+        startActivityForResult(mediaChooser, REQUEST_CODE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        // Check which request we're responding to
+        if (requestCode == REQUEST_CODE) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                // The user picked a contact.
+                // The Intent's data Uri identifies which contact was selected.
+
+                // Do something with the contact here (bigger example below)
+                Uri contentUri = data.getData();
+
+                Log.d("contentUri", contentUri.toString());
+
+                getContentInfo(contentUri);
+            }
+        }
+    }
+
+    private void getContentInfo(Uri uri) {
+        // uri.getPath(); just open the file???
+        String mimeType = getContentResolver().getType(uri);
+
+        Cursor returnCursor =
+                getContentResolver().query(uri, null, null, null, null);
+    /*
+     * Get the column indexes of the data in the Cursor,
+     * move to the first row in the Cursor, get the data,
+     * and display it.
+     */
+        int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+        int sizeIndex = returnCursor.getColumnIndex(OpenableColumns.SIZE);
+        returnCursor.moveToFirst();
+
+        mFilenameDate.setText(returnCursor.getString(nameIndex));
+
+        mDurationDimension.setText(Long.toString(returnCursor.getLong(sizeIndex)));
     }
 
     @Override
