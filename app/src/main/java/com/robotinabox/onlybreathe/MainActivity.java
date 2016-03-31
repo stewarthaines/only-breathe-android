65dcdead73cd54be1aaee8634be6fd75a263b443
Stewart Haines <stewarthaines@gmail.com>
2016-03-31 18:12:55 +1100

    adds video playback with ExoPlayer

--- app/src/main/java/com/robotinabox/onlybreathe/MainActivity.java
+++ app/src/main/java/com/robotinabox/onlybreathe/MainActivity.java
@@ -1,6 +1,10 @@
 package com.robotinabox.onlybreathe;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.media.MediaMetadataRetriever;
@@ -10,31 +14,99 @@ import android.provider.OpenableColumns;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ActionMenuView;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.accessibility.CaptioningManager;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.exoplayer.AspectRatioFrameLayout;
+import com.google.android.exoplayer.ExoPlaybackException;
+import com.google.android.exoplayer.ExoPlayer;
+import com.google.android.exoplayer.MediaCodecTrackRenderer;
+import com.google.android.exoplayer.MediaCodecUtil;
+import com.google.android.exoplayer.MediaFormat;
+import com.google.android.exoplayer.audio.AudioCapabilities;
+import com.google.android.exoplayer.audio.AudioCapabilitiesReceiver;
+import com.google.android.exoplayer.drm.UnsupportedDrmException;
+import com.google.android.exoplayer.metadata.id3.Id3Frame;
+import com.google.android.exoplayer.text.CaptionStyleCompat;
+import com.google.android.exoplayer.text.Cue;
+import com.google.android.exoplayer.text.SubtitleLayout;
+import com.google.android.exoplayer.util.DebugTextViewHelper;
+import com.google.android.exoplayer.util.MimeTypes;
+import com.google.android.exoplayer.util.Util;
+import com.google.android.exoplayer.util.VerboseLogUtil;
+import com.robotinabox.onlybreathe.DemoPlayer.CaptionListener;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback, View.OnClickListener,
+        DemoPlayer.Listener,
+        AudioCapabilitiesReceiver.Listener {
+
+    private static final String TAG = "MainActivity";
 
     private Button mSelectVideoBtn;
     private static final int REQUEST_CODE = 1;
     private TextView mDurationDimension;
     private TextView mFilenameDate;
-    private ImageView mVideoThumbnail;
+    //private ImageView mVideoThumbnail;
+    private TextView playerStateTextView;
+    private boolean playerNeedsPrepare;
+    private AspectRatioFrameLayout videoFrame;
+    private SurfaceView surfaceView;
+
+    private DemoPlayer player;
+    private Uri contentUri;
+    private boolean enableBackgroundAudio = false;
+    private KeyCompatibleMediaController mediaController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View root = findViewById(R.id.root);
+
+        root.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    toggleControlsVisibility();
+                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    view.performClick();
+                }
+                return true;
+            }
+        });
+        root.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_BACK || keyCode == KeyEvent.KEYCODE_ESCAPE
+                        || keyCode == KeyEvent.KEYCODE_MENU) {
+                    return false;
+                }
+                return mediaController.dispatchKeyEvent(event);
+            }
+        });
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -59,7 +131,17 @@ public class MainActivity extends AppCompatActivity {
 
         mFilenameDate = (TextView) findViewById(R.id.filename_date);
 
-        mVideoThumbnail = (ImageView) findViewById(R.id.video_thumbnail);
+        //mVideoThumbnail = (ImageView) findViewById(R.id.video_thumbnail);
+
+        playerStateTextView = (TextView) findViewById(R.id.player_state_view);
+
+        videoFrame = (AspectRatioFrameLayout) findViewById(R.id.video_frame);
+        surfaceView = (SurfaceView) findViewById(R.id.surface_view);
+        surfaceView.getHolder().addCallback(this);
+
+        mediaController = new KeyCompatibleMediaController(this);
+        mediaController.setAnchorView(root);
+
     }
 
     private void selectVideoWithIntent() {
@@ -69,6 +151,20 @@ public class MainActivity extends AppCompatActivity {
         mediaChooser.setType("video/*");
         startActivityForResult(mediaChooser, REQUEST_CODE);
 
+    }
+
+    private void toggleControlsVisibility()  {
+        if (mediaController.isShowing()) {
+            mediaController.hide();
+//            debugRootView.setVisibility(View.GONE);
+        } else {
+            showControls();
+        }
+    }
+
+    private void showControls() {
+        mediaController.show(0);
+//        debugRootView.setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -82,12 +178,121 @@ public class MainActivity extends AppCompatActivity {
                 // The Intent's data Uri identifies which contact was selected.
 
                 // Do something with the contact here (bigger example below)
-                Uri contentUri = data.getData();
+                contentUri = data.getData();
 
                 Log.d("contentUri", contentUri.toString());
 
                 getContentInfo(contentUri);
+
+                showControls();
+
             }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+//        Intent intent = getIntent();
+        if (player == null) {
+            if (!maybeRequestPermission()) {
+                preparePlayer(true);
+            }
+        } else {
+            player.setBackgrounded(false);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (!enableBackgroundAudio) {
+            releasePlayer();
+        } else {
+            player.setBackgrounded(true);
+        }
+        //shutterView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //audioCapabilitiesReceiver.unregister();
+        releasePlayer();
+    }
+
+    /**
+     * Checks whether it is necessary to ask for permission to read storage. If necessary, it also
+     * requests permission.
+     *
+     * @return true if a permission request is made. False if it is not necessary.
+     */
+    @TargetApi(23)
+    private boolean maybeRequestPermission() {
+        if (requiresPermission(contentUri)) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @TargetApi(23)
+    private boolean requiresPermission(Uri uri) {
+        return Util.SDK_INT >= 23
+                && Util.isLocalFileUri(uri)
+                && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED;
+    }
+
+    private ExtractorRendererBuilder getRendererBuilder() {
+        String userAgent = Util.getUserAgent(this, "ExoPlayerDemo");
+//        switch (contentType) {
+//            case Util.TYPE_OTHER:
+                return new ExtractorRendererBuilder(this, userAgent, contentUri);
+//            default:
+//                throw new IllegalStateException("Unsupported type: " + contentType);
+//        }
+    }
+
+    private void preparePlayer(boolean playWhenReady) {
+        if (player == null) {
+            player = new DemoPlayer(getRendererBuilder());
+            player.addListener(this);
+//            player.setCaptionListener(this);
+//            player.setMetadataListener(this);
+//            player.seekTo(playerPosition);
+            playerNeedsPrepare = true;
+            mediaController.setMediaPlayer(player.getPlayerControl());
+            mediaController.setEnabled(true);
+/*            eventLogger = new EventLogger();
+            eventLogger.startSession();
+            player.addListener(eventLogger);
+            player.setInfoListener(eventLogger);
+            player.setInternalErrorListener(eventLogger);
+            debugViewHelper = new DebugTextViewHelper(player, debugTextView);
+            debugViewHelper.start();
+*/
+        }
+        if (playerNeedsPrepare) {
+            player.prepare();
+            playerNeedsPrepare = false;
+            updateButtonVisibilities();
+        }
+        player.setSurface(surfaceView.getHolder().getSurface());
+        player.setPlayWhenReady(playWhenReady);
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+/*            debugViewHelper.stop();
+            debugViewHelper = null;
+            playerPosition = player.getCurrentPosition();
+*/
+            player.release();
+            player = null;
+//            eventLogger.endSession();
+//            eventLogger = null;
         }
     }
 
@@ -120,7 +325,7 @@ public class MainActivity extends AppCompatActivity {
         int videoHeight = bmp.getHeight();
         int videoWidth = bmp.getWidth();
 
-        mVideoThumbnail.setImageBitmap(bmp);
+        //mVideoThumbnail.setImageBitmap(bmp);
 
         DateFormat df = new SimpleDateFormat("mm:ss");
         String formatted = df.format(new Date(timeInMillisec));
@@ -148,5 +353,145 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // DemoPlayer.Listener implementation
+
+    @Override
+    public void onStateChanged(boolean playWhenReady, int playbackState) {
+        String text = "playWhenReady=" + playWhenReady + ", playbackState=";
+        switch(playbackState) {
+            case ExoPlayer.STATE_BUFFERING:
+                text += "buffering";
+                break;
+            case ExoPlayer.STATE_ENDED:
+                text += "ended";
+                break;
+            case ExoPlayer.STATE_IDLE:
+                text += "idle";
+                break;
+            case ExoPlayer.STATE_PREPARING:
+                text += "preparing";
+                break;
+            case ExoPlayer.STATE_READY:
+                text += "ready";
+                break;
+            default:
+                text += "unknown";
+                break;
+        }
+        playerStateTextView.setText(text);
+        updateButtonVisibilities();
+    }
+
+    @Override
+    public void onError(Exception e) {
+        String errorString = null;
+        if (e instanceof ExoPlaybackException
+                && e.getCause() instanceof MediaCodecTrackRenderer.DecoderInitializationException) {
+            // Special case for decoder initialization failures.
+            MediaCodecTrackRenderer.DecoderInitializationException decoderInitializationException =
+                    (MediaCodecTrackRenderer.DecoderInitializationException) e.getCause();
+            if (decoderInitializationException.decoderName == null) {
+                if (decoderInitializationException.getCause() instanceof MediaCodecUtil.DecoderQueryException) {
+                    errorString = getString(R.string.error_querying_decoders);
+                } else if (decoderInitializationException.secureDecoderRequired) {
+                    errorString = getString(R.string.error_no_secure_decoder,
+                            decoderInitializationException.mimeType);
+                } else {
+                    errorString = getString(R.string.error_no_decoder,
+                            decoderInitializationException.mimeType);
+                }
+            } else {
+                errorString = getString(R.string.error_instantiating_decoder,
+                        decoderInitializationException.decoderName);
+            }
+        }
+        if (errorString != null) {
+            Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_LONG).show();
+        }
+        playerNeedsPrepare = true;
+        updateButtonVisibilities();
+    }
+
+    @Override
+    public void onVideoSizeChanged(int width, int height, int unappliedRotationDegrees,
+                                   float pixelWidthAspectRatio) {
+        videoFrame.setAspectRatio(
+                height == 0 ? 1 : (width * pixelWidthAspectRatio) / height);
+    }
+
+    // User controls
+
+    private void updateButtonVisibilities() {
+/*
+        retryButton.setVisibility(playerNeedsPrepare ? View.VISIBLE : View.GONE);
+        videoButton.setVisibility(haveTracks(DemoPlayer.TYPE_VIDEO) ? View.VISIBLE : View.GONE);
+        audioButton.setVisibility(haveTracks(DemoPlayer.TYPE_AUDIO) ? View.VISIBLE : View.GONE);
+        textButton.setVisibility(haveTracks(DemoPlayer.TYPE_TEXT) ? View.VISIBLE : View.GONE);
+*/
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        if (player != null) {
+            player.setSurface(holder.getSurface());
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // Do nothing.
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (player != null) {
+            player.blockingClearSurface();
+        }
+    }
+
+    @Override
+    public void onAudioCapabilitiesChanged(AudioCapabilities audioCapabilities) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    private static final class KeyCompatibleMediaController extends MediaController {
+
+        private MediaController.MediaPlayerControl playerControl;
+
+        public KeyCompatibleMediaController(Context context) {
+            super(context);
+        }
+
+        @Override
+        public void setMediaPlayer(MediaController.MediaPlayerControl playerControl) {
+            super.setMediaPlayer(playerControl);
+            this.playerControl = playerControl;
+        }
+
+        @Override
+        public boolean dispatchKeyEvent(KeyEvent event) {
+            int keyCode = event.getKeyCode();
+            if (playerControl.canSeekForward() && keyCode == KeyEvent.KEYCODE_MEDIA_FAST_FORWARD) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                    playerControl.seekTo(playerControl.getCurrentPosition() + 15000); // milliseconds
+                    show();
+                }
+                return true;
+            } else if (playerControl.canSeekBackward() && keyCode == KeyEvent.KEYCODE_MEDIA_REWIND) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                    playerControl.seekTo(playerControl.getCurrentPosition() - 5000); // milliseconds
+                    show();
+                }
+                return true;
+            }
+            return super.dispatchKeyEvent(event);
+        }
     }
 }
